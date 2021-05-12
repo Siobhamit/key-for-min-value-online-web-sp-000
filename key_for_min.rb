16f6dff193f90692ee843b1302fa9780ae22fa08
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-12 01:22:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+
   values = []
-  name_hash each do |key, value|
+  name_hash.each do |key, value|
     values << value
   end
     while values.length > 1
