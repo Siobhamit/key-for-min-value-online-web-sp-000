5bb541d3a5dafa2d479d63fbf3fed80b46f2d50d
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-12 00:46:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    values = []
+name_hash.each do |key, value|
+    values << value
+  end
+   values.each do |a, b|
+     if a > b
+    [a, b]
+  else [b, a]
+  end
+end
+name_hash.key(values[0])
 end
